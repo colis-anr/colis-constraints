7ffe5f7bc7453d3b66f18628e9f6ae4a100d9e7f
Niols <niols@niols.fr>
2021-06-12 12:24:37 +0200

    documentation

--- src/efficient/colis_constraints_efficient.mli
+++ src/efficient/colis_constraints_efficient.mli
@@ -15,24 +15,47 @@ type t
 val true_ : t
 (** The true constraint. *)
 
+(** {3 Basic Constraints} *)
+
 val eq : var -> var -> t
+(** The base constraint representing the equality between two variables. *)
+
 val neq : var -> var -> t
+(** Same as {!eq} for disequality. *)
 
 val feat : var -> feat -> var -> t
+(** Same as {!eq} for feature constraint. *)
+
 val nfeat : var -> feat -> var -> t
+(** Same as {!eq} for negated feature constraint. *)
 
 val abs : var -> feat -> t
+(** Same as {!eq} for absence constraint. *)
+
 val nabs : var -> feat -> t
+(** Same as {!eq} for negated absence constraint. *)
 
 val maybe : var -> feat -> var -> t
+(** Same as {!eq} for maybe constraint. [maybe x f y] is equivalent to [abs x f
+    ∨ feat x f y] but the representation is more efficient. *)
+
 val nmaybe : var -> feat -> var -> t
+(** Same as {!eq} for negated maybe constraints. *)
 
 val fen : var -> feat_set -> t
+(** Same as {!eq} for fence constraints. *)
+
 val nfen : var -> feat_set -> t
+(** Same as {!eq} for negated fence constraints. *)
 
 val sim : var -> feat_set -> var -> t
+(** Same as {!eq} for similarity constraints. *)
+
 val nsim : var -> feat_set -> var -> t
+(** Same as {!eq} for negated similarity constraints. *)
+
 val sim1 : var -> feat -> var -> t
+(** Same as {!sim} for a singleton set. *)
 
 val reg : var -> t
 val nreg : var -> t
@@ -48,17 +71,37 @@ val char : var -> t
 val nchar : var -> t
 val symlink : var -> t
 val nsymlink : var -> t
+(** Same as {!eq} for the various file kinds constraints, negated or not. *)
+
+val literal : Literal.t -> t
+(** Build a constraint from a common literal. *)
+
+(** {3 Macros} *)
 
 val resolve : var -> Path.normal -> Path.t -> var -> t
 val noresolve : var -> Path.normal -> Path.t -> t
 val maybe_resolve : var -> Path.normal -> Path.t -> var -> t
 val similar : var -> var -> Path.normal -> Path.t -> var -> var -> t
 
-val literal : Literal.t -> t
+(** {3 Higher-order Constraints} *)
 
-val with_shadow_variables : (unit -> 'a) -> 'a
+val and_ : t -> t -> t
+val (&) : t -> t -> t
+(** Conjunction of two constraints, prefix and infix versions. *)
+
+val and_l : t list -> t
+(** Conjunction of a list of constraints. *)
+
+val or_ : t -> t -> t
+val or_l : t list -> t
+(** Disjunction of constraints. The disjunctions are always put in disjunctive
+   normal form, which makes their handling highly inefficient; beware! *)
 
 val exists  : (var -> t) -> t
+(** Existential quantification of constraints. {!exists} takes a function as
+   argument and gives it a fresh variable as argument. The function can use the
+   fresh variable to build a constraint. The variable is then quantified upon. *)
+
 val exists2 : (var -> var -> t) -> t
 val exists3 : (var -> var -> var -> t) -> t
 val exists4 : (var -> var -> var -> var -> t) -> t
@@ -67,17 +110,7 @@ val exists6 : (var -> var -> var -> var -> var -> var -> t) -> t
 val exists7 : (var -> var -> var -> var -> var -> var -> var -> t) -> t
 val exists8 : (var -> var -> var -> var -> var -> var -> var -> var -> t) -> t
 val exists9 : (var -> var -> var -> var -> var -> var -> var -> var -> var -> t) -> t
-
-val and_ : t -> t -> t
-val (&) : t -> t -> t
-(** Conjunction of two raw literals, prefix and infix versions. *)
-
-val and_l : t list -> t
-(** Conjunction of a list of raw literals. *)
-
-val or_ : t -> t -> t
-val or_l : t list -> t
-(** Disjunction of raw literals. (Warning: inefficient.) *)
+(** Same as {!exists} for several variables at the same time. *)
 
 (** {2 Satisfiable Conjunctions} *)
 
@@ -86,6 +119,7 @@ type sat_conj
     of this type are satisfiable. *)
 
 val true_sat_conj : sat_conj
+(** The empty satisfiable conjunction, trivially true. *)
 
 val add_to_sat_conj : t -> sat_conj -> sat_conj Dnf.t
 (** Add a constraint of type {!t} to a satisfiable conjunction. This yields a
@@ -100,3 +134,7 @@ val pp_sat_conj : Format.formatter -> sat_conj -> unit
 val pp_sat_conj_as_dot : name:string -> Format.formatter -> sat_conj -> unit
 
 val sat_conj_to_literals : sat_conj -> Literal.t Seq.t
+
+(** {2 Undocumented} *)
+
+val with_shadow_variables : (unit -> 'a) -> 'a
